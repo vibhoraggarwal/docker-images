eee79978419b322d3eb7cd9aaa33b3d8e96e4ce3
vibhoraggarwal <vibhorag24@gmail.com>
2025-09-26 01:15:49 +0200

    code: adding fallback uid

--- coverletter/entrypoint.sh
+++ coverletter/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+set -e
 # dynamically create user at runtime
-groupadd -g ${USER_GID} ${USERNAME} 2>/dev/null || true
-useradd -m -u ${USER_UID} -g ${USER_GID} ${USERNAME} 2>/dev/null || true
+
+# Create group if not exists
+if ! getent group $USERNAME >/dev/null; then
+    groupadd -g ${USER_GID:-1000} $USERNAME
+fi
+
+# Create user if not exists
+if ! id -u $USERNAME >/dev/null 2>&1; then
+    useradd -m -u ${USER_UID:-1000} -g ${USER_GID:-1000} -s /bin/bash $USERNAME
+fi
 exec gosu ${USERNAME} "$@"
